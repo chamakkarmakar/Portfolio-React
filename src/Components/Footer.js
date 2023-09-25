120f8c77561f8062438f865e630b6906b07cab25
Chamak Karmakar <chamakkarmakar.ck@gmail.com>
2023-09-25 13:17:14 +0600

    Updated portfolio

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,18 +1,28 @@
 import { FaFacebookSquare, FaInstagramSquare, FaLinkedin } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const Footer = () => {
     return (
         <footer class="footer footer-center p-10 bg-base-200 text-base-content rounded">
             <div className="flex flex-col items-center justify-center">
                 <div class="grid grid-cols-3 gap-4">
-                    <FaFacebookSquare className='w-6 h-6'></FaFacebookSquare>
+                    <Link to="https://www.facebook.com/chamak.karmakar/">
+                        <FaFacebookSquare className='w-6 h-6'></FaFacebookSquare>
+                    </Link>
+                    <Link to="https://www.facebook.com/chamak.karmakar/">
                     <FaInstagramSquare className="w-6 h-6"></FaInstagramSquare>
+                    </Link>
+                    <Link to="https://www.linkedin.com/in/chamak-karmakar-911522176/">
                     <FaLinkedin className="w-6 h-6"></FaLinkedin>
+                    </Link>
+
+                    
+                    
                 </div>
                 <div>
-                <p>Copyright © 2022 - Chamak Karmakar</p>
+                    <p>Copyright © {(new Date().getFullYear())} - Chamak Karmakar</p>
+                </div>
             </div>
-            </div>
-            
+
         </footer>
     );
 };
